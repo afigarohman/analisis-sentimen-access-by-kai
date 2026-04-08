6dac3cfc3bf70d47de707126bc5fd0b858a76773
afiga rohman <168763994+afigarohman@users.noreply.github.com.>
2026-04-08 15:46:53 +0700

    feat: menambahkan routing dan struktur dasar aplikasi

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: DashboardView,
     },
     {
       path: '/about',
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/feedback',
+      name: 'feedback',
+      component: () => import('../views/FeedbackView.vue'),
+    }
   ],
 })
 
